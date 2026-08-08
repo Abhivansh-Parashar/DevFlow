a2dafe696a371b0f8f906cf7d28cf8e27a9d25d9
Abhivansh Parashar <abhivanshparashar@gmail.com>
2026-08-08 20:20:52 +0530

    file name changed

--- Backend/src/main/java/org/devflow/DevFlow.java
+++ Backend/src/main/java/org/devflow/DevFlow.java
@@ -1,13 +1,13 @@
-package org.devflow.devflow;
+package org.devflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class DevFlow {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(DevFlow.class, args);
     }
 
 }
