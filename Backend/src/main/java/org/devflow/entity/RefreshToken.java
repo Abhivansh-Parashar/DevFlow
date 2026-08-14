cad312f464dff9394dbf3b3a03b5301b4b0f2e8e
Abhivansh Parashar <abhivanshparashar@gmail.com>
2026-08-14 18:38:25 +0530

    Added refreshToken entity

---
+++ Backend/src/main/java/org/devflow/entity/RefreshToken.java
@@ -0,0 +1,42 @@
+package org.devflow.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "token_hash", nullable = false, unique = true)
+    private String tokenHash;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(name = "revoked_at")
+    private LocalDateTime revokedAt;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void setCreatedAt(){
+        createdAt = LocalDateTime.now();
+    }
+}
--- Backend/src/main/java/org/devflow/entity/User.java
+++ Backend/src/main/java/org/devflow/entity/User.java
@@ -3,29 +3,39 @@ package org.devflow.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.devflow.enums.Role;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "app_user")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     @NotBlank
     private String name;
+
     @Email
     @NotBlank
+    @Column(unique = true)
     private String email;
+
     @NotBlank
     private String passwordHash;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
     private String avatar;
     private boolean online;
     private LocalDateTime createdAt;
@@ -36,9 +46,9 @@ public class User {
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
     }
+
     @PreUpdate
     private void onUpdate() {
         updatedAt = LocalDateTime.now();
     }
-
 }
