beb2aa0334ff79209f567283192881f8b97f1673
Abhivansh Parashar <abhivanshparashar@gmail.com>
2026-09-05 10:19:35 +0530

    Added required user dtos

---
+++ Backend/src/main/java/org/devflow/user/dto/AvatarUploadResponse.java
@@ -0,0 +1,10 @@
+package org.devflow.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AvatarUploadResponse {
+    private String avatarUrl;
+}
---
+++ Backend/src/main/java/org/devflow/user/dto/UserDto.java
@@ -0,0 +1,14 @@
+package org.devflow.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String avatar;
+    private boolean online;
+}
