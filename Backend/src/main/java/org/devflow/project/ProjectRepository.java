c3b0d45ae658d1f40ab1dc026914435ff2b98a2f
Abhivansh Parashar <abhivanshparashar@gmail.com>
2026-09-05 10:21:20 +0530

    fixed a bug in reaction repository

--- Backend/src/main/java/org/devflow/project/ProjectRepository.java
+++ Backend/src/main/java/org/devflow/project/ProjectRepository.java
@@ -1,9 +1,11 @@
 package org.devflow.project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project , Long> {
     List<Project> findByWorkspaceId(Long workspaceId);
     boolean existsByWorkspaceIdAndKeyPrefix(Long workspaceId, String prefix);
