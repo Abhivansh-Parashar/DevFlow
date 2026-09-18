90c9b43e27ae9c7393162f5ebfc59fca2a52b11f
Abhivansh Parashar <abhivanshparashar@gmail.com>
2026-09-18 18:31:03 +0530

    Addec analytics service

--- Backend/src/main/java/org/devflow/issue/LinkedCommitRepository.java
+++ Backend/src/main/java/org/devflow/issue/LinkedCommitRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface LinkedCommitRepository extends JpaRepository<LinkedCommit, UUID> {
+public interface LinkedCommitRepository extends JpaRepository<LinkedCommit, Long> {
+    long countByIssueWorkspaceId(Long workspaceId);
 }
