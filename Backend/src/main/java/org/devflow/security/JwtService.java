9c47114e889fe0587bfbb21134916a05598ba6aa
Abhivansh Parashar <abhivanshparashar@gmail.com>
2026-09-21 20:56:05 +0530

    completed jwt service

--- Backend/src/main/java/org/devflow/security/JwtService.java
+++ Backend/src/main/java/org/devflow/security/JwtService.java
@@ -1,8 +1,58 @@
 package org.devflow.security;
 
 
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.devflow.user.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
 
 @Service
 public class JwtService {
+    private final SecretKey secretKey;
+
+    public JwtService(@Value("${jwt.secret}") String jwtSecret) {
+        this.secretKey = Keys.hmacShaKeyFor(
+                jwtSecret.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public String generateAccessToken(User user){
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + 15 * 60 * 1000);
+
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(now)
+                .expiration(expiry)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public String generateRefreshToken(){
+        byte[] bytes = new byte[32];
+        new SecureRandom().nextBytes(bytes);
+
+        return Base64.getUrlEncoder()
+                .withoutPadding()
+                .encodeToString(bytes);
+    }
+
+
 }
