268907732f68f7064c93c07cbba520c4f07c6f58
Abhivansh Parashar <abhivanshparashar@gmail.com>
2026-09-22 09:55:23 +0530

    added refresh token and auth response

--- Backend/src/main/java/org/devflow/auth/RefreshToken.java
+++ Backend/src/main/java/org/devflow/auth/RefreshToken.java
@@ -1,10 +1,7 @@
 package org.devflow.auth;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.devflow.user.User;
 
 import java.time.LocalDateTime;
@@ -15,6 +12,7 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class RefreshToken {
 
     @Id
